d21bccaf66c6034689b103f3cc272db09d318f03
amyvgs <amyvud1@gmail.com>
2025-01-14 11:13:02 -0500

    Server Changes

--- server/server.js
+++ server/server.js
@@ -26,4 +26,5 @@ app.use("/api/manage", manageRoutes);
 
 
 // start server 
-app.listen(3000, () => console.log('Server running on localhost:3000'));
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
